b11fde2eb9783ee23d97e384c9ed91b7ed852a67
Pramod Jacob <psjacob@Pramods-MacBook-Air.local>
2024-11-02 13:16:06 -0400

    Add spell-checking to MD files

--- init.lua
+++ init.lua
@@ -92,6 +92,14 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   end,
 })
 
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = '*.md',
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = 'en_us'
+  end,
+})
+
 -- [[ PLUGINS ]]
 
 -- Install lazy.nvim plugin manager
