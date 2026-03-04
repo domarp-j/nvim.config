36cf979b8e93efc8488602f693843ee3dc6a4248
Pramod Jacob <pjacob@sewerai.com>
2026-03-04 13:50:18 -0500

    Enable neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,30 +1,27 @@
 -- neo-tree.nvim
 -- File system browser sidebar. Press \ to reveal the current file in the tree; press \ again inside the tree to close it.
 
--- Optional: uncomment to enable
--- return {
---   'nvim-neo-tree/neo-tree.nvim',
---   version = '*',
---   dependencies = {
---     'nvim-lua/plenary.nvim',
---     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
---     'MunifTanjim/nui.nvim',
---   },
---   lazy = false,
---   keys = {
---     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
---   },
---   ---@module 'neo-tree'
---   ---@type neotree.Config
---   opts = {
---     filesystem = {
---       window = {
---         mappings = {
---           ['\\'] = 'close_window',
---         },
---       },
---     },
---   },
--- }
-
-return {}
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  lazy = false,
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  ---@module 'neo-tree'
+  ---@type neotree.Config
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
