765ab0b47579856e05e4a130fa0076fa955b6225
Pramod Jacob <pjacob@sewerai.com>
2026-03-04 14:13:19 -0500

    Add vim-fugitive

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,6 @@
+-- vim-fugitive
+-- Git integration for Neovim. Run any git command via :Git (or :G), stage and diff files with :Gwrite/:Gdiffsplit, and view annotated blame with :Git blame.
+
+return {
+  'tpope/vim-fugitive',
+}
