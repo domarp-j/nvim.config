6527edd4dac78d9fa027858433d769519690113b
Pramod Jacob <pjacob@sewerai.com>
2026-03-04 12:59:56 -0500

    Move plugins into dedicated files

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,11 @@
+-- autopairs
+-- https://github.com/windwp/nvim-autopairs
+
+-- Optional: uncomment to enable
+-- return {
+--   'windwp/nvim-autopairs',
+--   event = 'InsertEnter',
+--   opts = {},
+-- }
+
+return {}
