9ed501ca3f802788ff8e42a58d02814d77c5d0f2
Pramod Jacob <pjacob@sewerai.com>
2026-04-16 17:25:38 -0400

    Enable hidden file search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -71,7 +71,10 @@ return {
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
-        -- pickers = {}
+        pickers = {
+          find_files = { hidden = true },
+          live_grep = { additional_args = function() return { '--hidden' } end },
+        },
         extensions = {
           ['ui-select'] = { require('telescope.themes').get_dropdown() },
         },
